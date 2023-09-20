54c905d24224d2c6434f0aeee4dc601ffed46259
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-09-20 23:55:52 +0100

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,16 +3,8 @@
 use CodebarAg\Zendesk\Tests\TestCase;
 use Illuminate\Support\Facades\Event;
 
-uses(TestCase::class)->in(__DIR__);
-
-uses()->beforeEach(function () {
-    Event::fake();
-})->in(__DIR__);
-
-uses()->beforeAll(function () {
-    if (is_dir('Fixtures/Saloon') && count(scandir('Fixtures/Saloon')) > 0) {
-        config()->set('zendesk.subdomain', 'codebar-zendesk');
-        config()->set('zendesk.auth.email_address', 'fake-email');
-        config()->set('zendesk.auth.api_token', 'fake-token');
-    }
-})->in(__DIR__);
+uses(TestCase::class)
+    ->beforeEach(function () {
+        Event::fake();
+    })
+    ->in(__DIR__);
