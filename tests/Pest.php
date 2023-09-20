e28f77c398d7df103e10295cafa999fd28937bc7
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-09-20 23:38:16 +0100

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -8,3 +8,9 @@ uses(TestCase::class)->in(__DIR__);
 uses()->beforeEach(function () {
     Event::fake();
 })->in(__DIR__);
+
+uses()->beforeAll(function () {
+    config()->set('zendesk.subdomain', 'codebar-zendesk');
+    config()->set('zendesk.credentials.email', 'fake-email');
+    config()->set('zendesk.credentials.token', 'fake-token');
+})->in(__DIR__);
