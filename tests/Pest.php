7cd5d7e7352b45f2ca8248f3b5b1a93944ddcd57
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-09-20 23:42:03 +0100

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,7 +10,9 @@ uses()->beforeEach(function () {
 })->in(__DIR__);
 
 uses()->beforeAll(function () {
-    config()->set('zendesk.subdomain', 'codebar-zendesk');
-    config()->set('zendesk.credentials.email', 'fake-email');
-    config()->set('zendesk.credentials.token', 'fake-token');
+    if (is_dir('Fixtures/Saloon') && count(scandir('Fixtures/Saloon')) > 0) {
+        config()->set('zendesk.subdomain', 'codebar-zendesk');
+        config()->set('zendesk.credentials.email', 'fake-email');
+        config()->set('zendesk.credentials.token', 'fake-token');
+    }
 })->in(__DIR__);
